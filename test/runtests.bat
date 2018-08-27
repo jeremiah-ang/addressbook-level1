5d0ebf1eae3249cd6e810ad1bd7d5790635f4f22
jeremiah-ang <jeremiahang94@gmail.com>
2018-08-27 23:14:20 +0800

    Update test cases

--- test/runtests.bat
+++ test/runtests.bat
@@ -1,8 +1,5 @@
 @ECHO OFF
 
-REM change current directory to the location of this script
-pushd %~dp0
-
 REM create bin directory if it doesn't exist
 if not exist ..\bin mkdir ..\bin
 
@@ -10,8 +7,6 @@ REM compile the code into the bin folder
 javac  ..\src\seedu\addressbook\Addressbook.java -d ..\bin
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE ********** 
-    REM return to previous directory
-    popd
     exit /b 1
 )
 REM no error here, errorlevel == 0
@@ -33,6 +28,3 @@ java -classpath ..\bin seedu.addressbook.AddressBook < input.txt >> actual.txt
 
 REM compare the output to the expected output
 FC actual.txt expected.txt
-
-REM return to previous directory
-popd
